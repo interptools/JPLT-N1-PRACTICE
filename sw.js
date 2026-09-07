cf27515bbf16b4fd5d33c651d44b0795239a3f9e
interptools <areymar36@gmail.com>
2026-09-07 12:18:51 +0900

    Update sw.js

--- sw.js
+++ sw.js
@@ -13,11 +13,11 @@ const SHELL_FILES = [
   './',
   './index.html',
   './manifest.webmanifest',
-  './icons/icon-192.png',
-  './icons/icon-512.png',
-  './icons/icon-maskable-512.png',
-  './icons/apple-touch-icon.png',
-  './icons/favicon-32.png'
+  './icon-192.png',
+  './icon-512.png',
+  './icon-maskable-512.png',
+  './apple-touch-icon.png',
+  './favicon-32.png'
 ];
 
 const LIB = 'https://cdn.jsdelivr.net/npm/hanzi-writer@3/dist/hanzi-writer.min.js';
